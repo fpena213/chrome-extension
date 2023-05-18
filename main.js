2cbf2289fb9c01a2daee33cc2c5e020d0ecc0249
fbiswas <fpena213@gmail.com>
2023-05-17 23:17:40 -0700

    change pic on click, added icons

--- main.js
+++ main.js
@@ -10,6 +10,7 @@
 // const fakePic = document.getElementById('#cs-logo').src="https://imgflip.com/i/7m76ef";
 // document.appendChild(fakePic);
 
+
 const picArry = ['https://i.imgflip.com/7m791m.jpg', 'https://i.imgflip.com/7m793c.jpg', 'https://i.imgflip.com/7m795r.jpg','https://i.imgflip.com/7m793k.jpg', 'https://i.imgflip.com/7m797n.jpg', 'https://i.imgflip.com/7m7990.jpg','https://i.imgflip.com/7m799i.jpg', 'https://i.imgflip.com/7m79d9.jpg', 'https://i.imgflip.com/7m79ga.jpg', 'https://i.imgflip.com/7m79fm.jpg','https://i.imgflip.com/7m79no.jpg']
 
 let randomNum = Math.floor(Math.random() * picArry.length);
@@ -20,4 +21,12 @@ const pic = document.createElement('img')
 pic.setAttribute('id','pic')
 pic.setAttribute('src',`${picArry[randomNum]}`)
 //body.appendChild(pic)
-codeBox.replaceWith(pic)
+codeBox.replaceWith(pic);
+//get it to refresh with clicking button with event listener
+document.addEventListener('click', function() {
+    randomNum = Math.floor(Math.random() * picArry.length);
+    pic.setAttribute('src',`${picArry[randomNum]}`);
+    codeBox.replaceWith(pic);
+});
+//add more to array
+//add logo
