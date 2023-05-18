139b2aa0643f6c41f1fe7a72f46bcdb21990a8ec
fbiswas <fpena213@gmail.com>
2023-05-17 22:13:17 -0700

    added assets

--- main.js
+++ main.js
@@ -6,9 +6,16 @@
   
 // });
 
-console.log('hey')
-  
-const codeBox = document.querySelector('#editor-side')
-codeBox.remove()
 
-//cid = editor-side
+// const fakePic = document.getElementById('#cs-logo').src="https://imgflip.com/i/7m76ef";
+// document.appendChild(fakePic);
+
+const picArry = ['assets/memes/']
+
+const codeBox = document.querySelector('#qd-content');
+codeBox.remove()
+const body = document.querySelector('body')
+const pic = document.createElement('img')
+pic.setAttribute('id','pic')
+pic.setAttribute('src','https://i.imgflip.com/7m78qv.jpg')
+body.appendChild(pic)
