0c19fd6b217220696cf5f09ad19f7a880b9a4fcb
fbiswas <fpena213@gmail.com>
2023-05-17 21:02:09 -0700

    added manifest, main.js file, assets folder

---
+++ main.js
@@ -0,0 +1,14 @@
+// document.addEventListener('DOMContentLoaded', () => {
+//   console.log('hey')
+  
+//   const body = document.querySelector('body');
+//   body.remove();
+  
+// });
+
+console.log('hey')
+  
+const codeBox = document.querySelector('#editor-side')
+codeBox.remove()
+
+//cid = editor-side
